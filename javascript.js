5a4b73805059b7573eb284819be2e45485eba895
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-06-29 00:08:25 -0400

    Initial commit, this will the game to javascript.

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function rockPaperScissors(handSign) {
+    let ai = Math.floor(Math.random() * 3);
+    switch (ai) {
+        case 0:
+            ai = "Rock"
+            break;
+        case 1:
+            ai = "Paper"
+            break;
+        case 2:
+            ai = "Scissors"
+            break;    
+    }
+    if (handSign !== "Rock" && handSign !== "Paper" && handSign !== "Scissors") {
+        return `Error! ${handSign} is an invalid input!`
+    }else if (handSign === "Rock" && ai === "Scissors") {
+        return `You win! You chose ${handSign}. And the AI chose ${ai}.`
+    }else if (handSign === "Paper" && ai === "Rock") {
+        return `You win! You chose ${handSign}. And the AI chose ${ai}.`
+    } else if (handSign === "Scissors" && ai === "Paper") {
+     return `You win! You chose ${handSign}. And the AI chose ${ai}.`
+    } else if (handSign === ai) {
+        return `Its a tie! You chose ${handSign}. And the AI chose ${ai}.`
+    }else {
+        return `"You lose! You chose ${handSign}. And the AI chose ${ai}.`
+    }
+}
+
+console.log(rockPaperScissors(1))
