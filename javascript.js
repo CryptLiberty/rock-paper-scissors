0f54971975cc1a17595bdcca11b5adae3e2ec936
CryptLiberty <130183977+CryptLiberty@users.noreply.github.com>
2023-06-29 02:44:59 -0400

    Reworks the playGame func, also adds game func The playGame function worked fine by it self, but caused problems down the road. This commit reworks the playGame function to better work with the new game function.

--- javascript.js
+++ javascript.js
@@ -23,21 +23,57 @@ let playerSelection = () => {
     return userInput
 };
 
-function playGame(playerSelection, computerSelection) {
+let playGame = (playerSelection, computerSelection) => {
        // Actual 'Game' portion, does a series of if statements to determine if the player won or loss.
     if (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") {
-        return `Error! ${playerSelection} is an invalid input!`
+        return 4
     }else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return `You win! You chose ${playerSelection}. And the AI chose ${computerSelection}.`
+        return 1
     }else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return `You win! You chose ${playerSelection}. And the AI chose ${computerSelection}.`
+        return 1
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-     return `You win! You chose ${playerSelection}. And the AI chose ${computerSelection}.`
-    } else if (playerSelection === computerSelection) {
-        return `Its a tie! You chose ${playerSelection}. And the AI chose ${computerSelection}.`
+     return 1
+    } else if (playerSelection = computerSelection) {
+        return 2
     }else {
-        return `"You lose! You chose ${playerSelection}. And the AI chose ${computerSelection}.`
+        return 3
+    };
+}
+
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    let loopNumber = 0
+    while (loopNumber < 5){
+        console.log(loopNumber)
+        let playerChoice = playerSelection()
+        let computerChoice = computerSelection()
+        let gameResult = playGame(playerChoice, computerChoice)
+        if (gameResult == 1) {
+            playerScore++
+            alert(`YOU WON! The Computer chose ${computerChoice}, and you chose ${playerChoice}.\nCurrent Score: Player Score:${playerScore} Computer Score:${computerScore}`)
+        }else if (gameResult == 3) {
+            computerScore++
+            alert(`YOU LOST! The Computer chose ${computerChoice}, and you chose ${playerChoice}.\nCurrent Score: Player Score:${playerScore} Computer Score:${computerScore}`)
+            
+        }else if (gameResult == 2) {
+            alert(`Alert! You both chose ${playerChoice}, resulting in a TIE. Restarting round.`)
+            continue;
+
+        }else if (gameResult == 4) {
+            alert(`Error! ${playerChoice} is an invalid selection! Aborting game.`)
+            break;
+        }
+        loopNumber++
+    }
+    if (playerScore > computerScore) {
+        alert(`YOU WON! Your score of ${playerScore} is higher than the computer's score of ${computerScore}`)
+    }else if (playerScore < computerScore) {
+        alert(`YOU LOST! Your score of ${playerScore} is lower than the computer's score of ${computerScore}`)
+    }else {
+        alert(`Error, game was no longer able to continue due to an error.`)
     }
 }
 
-alert(playGame(playerSelection(), computerSelection()))
+game()
